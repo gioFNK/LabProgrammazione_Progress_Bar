14d0c7564796bafdf7fd1d15b4cbb2c9f1cc486e
gioFNK <gio.fnk@gmail.com>
2019-10-19 12:05:25 +0200

    Testing curses libs and adding files needing them

---
+++ cmake-build-debug/CMakeFiles/core.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/core.dir/main.cpp.obj"
+  "CMakeFiles/core.dir/BarGUI.cpp.obj"
+  "CMakeFiles/core.dir/FileManager.cpp.obj"
+  "CMakeFiles/core.dir/ResourceFile.cpp.obj"
+  "CMakeFiles/core.dir/ProgressBarDisplay.cpp.obj"
+  "libcore.pdb"
+  "libcore.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
